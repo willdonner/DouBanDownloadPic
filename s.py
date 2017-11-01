98882c5ab41b9e5cd1f7de89d7ad54672cd26922
willdonner <765019300@qq.com>
2017-11-01 12:37:36 +0800

    s.py is matplotlib

--- s.py
+++ s.py
@@ -1,3 +1,14 @@
-import downpic
-class s():
-    beauty = downpic.BeautifulSoupPic()
+import pandas as pd
+from numpy import *
+import matplotlib.pyplot as plt
+import nltk
+import re
+word_box =[]
+box = []
+with open('/Users/willdonner/DevsTest/result1.txt','r',encoding='utf-8') as file1:
+  inputfile = file1.read()
+  str2 = re.sub("[^A-Za-z\ ]", "", inputfile)
+  tokens = nltk.tokenize.word_tokenize(str2)
+  fd = nltk.FreqDist(tokens)
+  fd.plot(20,cumulative=False)
+  plt.show()
