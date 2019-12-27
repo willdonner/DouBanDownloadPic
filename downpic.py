5efc0fc0ed1335e13dfba56f76fd1e5a6587d5f1
willdonner(ysd) <765019300@qq.com>
2019-12-27 11:38:51 +0800

    bug fix add User-Agent

--- downpic.py
+++ downpic.py
@@ -1,3 +1,5 @@
+# !/usr/bin/python3
+# -*- coding: utf-8 -*-
 import requests
 from bs4 import BeautifulSoup
 import os
@@ -5,14 +7,16 @@ import time
 
 class BeautifulSoupPic():
     def __init__(self): ##初始化
-        #self.headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36'}
+        self.headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36'}
         #self.web_url = 'https://www.douban.com/photos/album/127879998'
         #win下目录
-        self.folder_path = 'D:\BeautifulPicture'
+        # self.folder_path = 'D:\BeautifulPicture'
         #mac下目录
-        #self.folder_path = '/Users/willdonner/DevsTest'
+        self.folder_path = '/Users/willdonner/DevsTest'
     def request(self, url): ##返回网页的请求
-        r = requests.get(url)
+        r = requests.get(
+            url=url,
+        headers=self.headers)
         return r
 
     def mkdir(self, path):  ##这个函数创建文件夹
@@ -55,11 +59,12 @@ class BeautifulSoupPic():
                 self.save_img(first_pos, img_name)
     def get_pic(self):
         #print('请输入你想下载的网址')
-        downlode_url = 'https://www.douban.com/photos/album/1632064232/'
+        downlode_url = 'https://www.douban.com/photos/album/127879998/'
         print(downlode_url)
         r = self.request(downlode_url)
         print('开始网页get请求')
         print('获取页数')
+        print(r.text)
         all_message = BeautifulSoup(r.text, 'lxml').find('div', class_='paginator')
         #for a_page in all_message:
         page = int(all_message.find('span', class_='thispage').get('data-total-page'))
